81eb656d255c081c7550a1b1657ce35d6c1879e9
Yego Chan <136900567+YegoChan@users.noreply.github.com>
2026-03-20 09:37:25 +0900

    2-2.c Update

---
+++ code/2-2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void swap(int *x, int *y) {
+    int swapper1 = x;
+    int swapper2 = y;
+    *x = swapper2;
+    *y = swapper1;
+    // *x = *x ^ *y;
+    // *y = *x ^ *y;
+    // *x = *x ^ *y;
+}
+
+int main() {
+    int x = 100, y = 200;
+
+    printf("x = %d, y = %d\n", x, y);
+    // TODO: swap 함수 호출
+    swap(&x, &y);
+    printf("x = %d, y = %d\n", x, y);
+
+    return 0;
+}
