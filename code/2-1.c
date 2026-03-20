c7d1bde07c900dca32fee18298ae4f2d09eaa159
Yego Chan <136900567+YegoChan@users.noreply.github.com>
2026-03-20 09:25:38 +0900

    2-1.c Update

---
+++ code/2-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void increment(int x) {
+    ++x;
+    printf("%d\n", x);
+}
+
+int main() {
+    int x = 10;
+
+    printf("%d\n", x);
+    increment(x);
+    printf("%d\n", x);
+}
+
+// x를 인수로 받는 increment의 증감연산자는, main frame의 int x의 값에는 영향을 주지 못한다.
